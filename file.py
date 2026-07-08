139675014f4c372eee8c453c6ff33c31e2cfef2d
Naveen prabu <naveenprabupanneer@gmail.com>
2026-07-08 23:08:24 +0530

    working with the locators in selenium

--- file.py
+++ file.py
@@ -1,14 +1,17 @@
-import selenium
 import time as t
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-a=webdriver.Edge()
+
+a = webdriver.Edge()
 a.maximize_window()
-a.get('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-a.find_element(By.ID).send_keys('')#use to give input values
-t.sleep(5)
-a.find_element(By.XPATH)
-t.sleep(10)
-a.find_element(By.CLASS_NAME).send_keys(''+Keys.ENTER)
-t.sleep(5)
+
+a.get("https://www.youtube.com/")
+
+sb = a.find_element(By.ID, "search")
+sb.send_keys("rick roll")
+sb.send_keys(Keys.ENTER)
+
+t.sleep(500)
+
+a.quit()
