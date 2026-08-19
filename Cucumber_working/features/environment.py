0b416b368ceb021c661575b089ed955a89460a9c
Naveen prabu <naveenprabupanneer@gmail.com>
2026-08-19 11:25:37 +0530

    login using cucumber

--- Cucumber_working/features/environment.py
+++ Cucumber_working/features/environment.py
@@ -1,6 +1,12 @@
 from selenium import webdriver
-def before(context):
-    context.driver=webdriver.edge()
+
+
+def before_all(context):
+    print("Starting browser...")
+    context.driver = webdriver.Edge()
     context.driver.maximize_window()
-def after(context):
+
+
+def after_all(context):
+    print("Closing browser...")
     context.driver.quit()
